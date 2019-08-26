c99469918dc22b6f2ebc5986cd1ff0562c785ebf
ZYYHsunshine <zzucgg@163.com>
2019-08-26 23:34:19 +0800

    v_1

--- src/main/java/tw/Main.java
+++ src/main/java/tw/Main.java
@@ -2,6 +2,9 @@ package tw;
 
 import tw.commands.GuessInputCommand;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by jxzhong on 2017/5/16.
  */
@@ -9,10 +12,25 @@ public class Main {
 
     private static final int RUN_TIMES = 6;
 
-    public static void main(String[] args) throws Exception {
+    public static Void main(String[] args) throws Exception {
+        String[] input = new String[4];
+        int sumA = 0, sumB = 0;
         for (int i = 0; i < RUN_TIMES; i++) {
-            String input = new GuessInputCommand().input();
-            System.out.println(input);
+            List<String> inputList = Arrays.asList(input);
+            String[] answer = {"6", "6", "6", "6"};
+            List<String> answerList = Arrays.asList(answer);
+            for (String number : inputList) {
+                if (answerList.contains(number)) {
+                    sumB++;
+                    if (answerList.indexOf(number) == inputList.indexOf(number)) {
+                        sumA++;
+                        sumB--;
+                    }
+                }
+            }
         }
+        System.out.println(sumA + "A" + sumB + "B");
     }
 }
+
+
--- src/main/java/tw/commands/GuessInputCommand.java
+++ src/main/java/tw/commands/GuessInputCommand.java
@@ -20,3 +20,5 @@ public class GuessInputCommand {
         return bufferedReader.readLine();
     }
 }
+
+
